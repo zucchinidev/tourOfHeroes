5dedc489634ef60017f165b16f31fed3ff80e725
Andrea Zucchini <azga84@gmail.com>
2016-09-15 00:49:37 +0200

    change service for use InMemoryWebApiModule and observables

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,15 +1,35 @@
 import {Injectable} from '@angular/core';
-import {HEROES} from './mock-heroes';
 import {Hero} from './hero.model';
-
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl: string = 'app/heroes';
+  constructor(private http: Http) {
+
+  }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.get(this.heroesUrl)
+      .toPromise()
+      .then((response) => response.json().data as Hero[])
+      .catch(this.handleError);
   }
 
   getHero(id: number): Promise<Hero> {
-    return Promise.resolve(HEROES.find((hero: Hero) =>  hero.id === id ));
+    return this.getHeroes()
+      .then((heroes) => heroes.find((hero: Hero) =>  hero.id === id ));
   }
+
+  private get(url: string): Observable {
+    return this.http.get(url);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('Error: ', error); // TODO only useful in the early stages of development. Change for system logs
+    return Promise.reject(error.message || error);
+  }
+
 }
