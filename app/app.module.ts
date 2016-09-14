ea8d0710ee79101f3da9ad4174ee60964ed85c49
Andrea Zucchini <azga84@gmail.com>
2016-09-15 00:50:24 +0200

    Inject InMemoryWebApiModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 
 import {AppComponent}  from './app.component';
@@ -9,11 +10,15 @@ import {HeroesComponent} from './heroes.component';
 import {HeroService} from './hero.service';
 import {DashBoardComponent} from './dashboard.component';
 import {routing} from './app.routing';
+import {InMemoryWebApiModule} from 'angular2-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     routing
   ],
   declarations: [
