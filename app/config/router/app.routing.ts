0acfc5bf4406dce644bba92da0303cda11708a82
Andrea Zucchini <azga84@gmail.com>
2016-09-16 21:33:52 +0200

    File structure change

--- app/config/router/app.routing.ts
+++ app/config/router/app.routing.ts
@@ -1,8 +1,8 @@
 import {Routes, RouterModule} from '@angular/router';
 import {HeroesComponent} from './heroes.component';
 import {ModuleWithProviders} from '@angular/core';
-import {DashBoardComponent} from './dashboard.component';
-import {HeroDetailComponent} from './hero-detail.component';
+import {DashBoardComponent} from './dashboard/dashboard.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 
 
 const appRoutes: Routes = [
