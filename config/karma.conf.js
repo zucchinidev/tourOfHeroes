98b6eb8d2f77b9b9076f46c7f16b8fbae9d8f308
Andrea Zucchini <azga84@gmail.com>
2016-10-21 23:19:34 +0200

    add karma config

---
+++ config/karma.conf.js
@@ -0,0 +1,40 @@
+var webpackConfig = require('./webpack.test');
+
+module.exports = function (config) {
+  var _config = {
+    basePath: '',
+
+    frameworks: ['jasmine'],
+
+    files: [
+      {pattern: './config/karma-test-shim.js', watched: false}
+    ],
+
+    // We don't precompile our TypeScript; Webpack transpiles our
+    // Typescript files on the fly, in memory, and feeds the emitted
+    // JS directly to Karma. There are no temporary files on disk.
+    preprocessors: {
+      './config/karma-test-shim.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: webpackConfig,
+
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+
+    webpackServer: {
+      noInfo: true
+    },
+
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['PhantomJS'],
+    singleRun: true
+  };
+
+  config.set(_config);
+};
