ef2cdf7e169a3bf892281fae756bca73744654ff
Andrea Zucchini <azga84@gmail.com>
2016-10-21 23:05:40 +0200

    move to src folder

--- src/app/root/app.component.ts
+++ src/app/root/app.component.ts
@@ -2,8 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/root/app.component.html',
-  styleUrls: ['app/root/app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 
 })
 export class AppComponent {
